4907504d6067999117e9af83b8c72887c0df5211
中村 <n.kosuko731@gmail.com>
2025-02-22 17:49:40 +0900

    memo

--- question_b/b_seat.rb
+++ question_b/b_seat.rb
@@ -1,19 +1,6 @@
-n = gets.to_i
-inputs = n.times.map{ gets.to_i }
-
-p gets.to_i.times.map do
-  x,u=gets.split
-  u=="JPY" ? x.to_f : x.to_f*380000
-end.sum
-
-
-# ---を|で区切って配列化、最初の
-inputs = gets.chomp.split('|').drop(1)
-len = []
-inputs.each do |i|
-  len << i.length
+n, d = gets.split.map(&:to_i)
+s = gets
+d.times do |i|
+  s[s.rindex("@")] = "."
 end
-puts len.join(' ')
-
-
-puts gets.chomp.split('|').map(&:length).drop(1)
+puts s
