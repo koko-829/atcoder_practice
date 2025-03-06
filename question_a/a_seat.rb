6b2543f8f161f989df38a386a610ee89aac07887
中村 <n.kosuko731@gmail.com>
2025-03-06 21:52:45 +0900

    memo

--- question_a/a_seat.rb
+++ question_a/a_seat.rb
@@ -3,5 +3,13 @@ len = a.uniq.length
 if len == 1
   puts a[1]
 elsif len == 2
-  
+  if a[0] == a[1]
+    puts a[2]
+  elsif a[0] == a[2]
+    puts a[1]
+  else
+    puts a[0]
+  end
+else
+  puts 0
 end
--- question_b/b_seat.rb
+++ question_b/b_seat.rb
@@ -1,1 +1,4 @@
+count = 0
 n = gets.to_i
+n.times { count += gets.chomp.match(/G C|P G|C P/) ? 1:0}
+puts count
