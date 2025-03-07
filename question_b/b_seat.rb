f59347b19554345221820813455ab3bcc8ebe032
中村 <n.kosuko731@gmail.com>
2025-03-08 01:03:45 +0900

    memo

--- question_b/b_seat.rb
+++ question_b/b_seat.rb
@@ -1,4 +1,2 @@
-count = 0
-n = gets.to_i
-n.times { count += gets.chomp.match(/G C|P G|C P/) ? 1:0}
-puts count
+cost = (gets.to_i * 1.08).floor
+puts cost > 206 ? ":(" : cost == 206 ? "so-so" : "Yay!"
